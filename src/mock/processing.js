067b0f33afc7b51359637273deb6ff999257090c
malinainsugar <super.a03-a2013@yandex.ru>
2023-06-07 20:58:29 +0500

    Добавление сортировки

--- src/mock/processing.js
+++ src/mock/processing.js
@@ -1,4 +1,4 @@
-import { FILTER_TYPE, SORT_TYPE } from '../const';
+import { FILTER_TYPE } from '../const';
 import dayjs from 'dayjs';
 
 const isPast = (pointDate) => dayjs(pointDate.dateFrom).isBefore(dayjs());
@@ -10,18 +10,6 @@ const filters = {
   [FILTER_TYPE.PAST]: (points) => points.filter((point) => isPast(point))
 };
 
-const getDifference = (oneDate, secondDate, param) => dayjs(secondDate).diff(oneDate, param);
-
-const sorting = {
-  [SORT_TYPE.DAY]: (points) => points.sort((prev,next) => getDifference(next.dateFrom, prev.dateFrom, '')),
-  // eslint-disable-next-line no-unused-vars
-  [SORT_TYPE.EVENT]: (points) => null,
-  [SORT_TYPE.TIME]: (points) => points.sort((prev, next) => getDifference(prev.dateFrom, prev.dateTo, 'minute') - getDifference(next.dateFrom, next.dateTo, 'minute')),
-  [SORT_TYPE.PRICE]: (points) => points.sort((prev, next) => prev.basePrice - next.basePrice),
-  // eslint-disable-next-line no-unused-vars
-  [SORT_TYPE.OFFERS]: (points) => null
-};
-
 const generateFilter = (points) => Object.entries(filters).map(
   ([filterName, filterPoints]) => ({
     name: filterName,
@@ -29,12 +17,5 @@ const generateFilter = (points) => Object.entries(filters).map(
   }),
 );
 
-const generateSorting = (points) => Object.entries(sorting).map(
-  ([sortedName, sortedPoints]) => ({
-    name: sortedName,
-    sequence: sortedPoints(points),
-  }),
-);
-
-export { generateFilter, generateSorting };
+export { generateFilter };
 

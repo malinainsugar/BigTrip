dad5fa12d3fcc0aed9698f360da87331f9e5303e
malinainsugar <super.a03-a2013@yandex.ru>
2023-06-10 19:20:25 +0500

    Реализует паттерн "Адаптер"

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -12,9 +12,26 @@ export default class PointsModel extends Observable {
     this.#pointsApiService = pointsApiService;
 
     this.#pointsApiService.points.then((points) => {
-      console.log(points);
+      console.log(points.map(this.#adaptToClient));
     });
   }
+
+  #adaptToClient = (point) => {
+    const adaptedPoint = {...point,
+      basePrice: point['base_price'],
+      dateFrom: point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
+      dateTo: point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      destinationId: point['is_favorite']
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedPoint['base_price'];
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['is_favorite'];
+
+    return adaptedPoint;
+  };
 
   init(points, destinations, offers) {
     this.#points = points;
